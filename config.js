88fed397b8b10d411cb10be84630d0691fb58c2e
Andy Mr Lit <shizxyandy+6@gmail.com>
2026-02-14 22:08:47 -0500

    Ajout/Mise à jour de config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// API Configuration
+// This file allows easy configuration of the external API URL
+
+module.exports = {
+    // External API URL - can be changed as needed
+    API_BASE_URL: process.env.EXTERNAL_API_URL || 'https://testmovieboxapi-ab2c4c4adb04.herokuapp.com'
+};
